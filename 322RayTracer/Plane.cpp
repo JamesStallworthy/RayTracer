1bbd5857c7e8bab2b7631da990eed2d3514d88b1
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-10-09 14:51:06 +0100

    Added Plane header

---
+++ 322RayTracer/Plane.cpp
@@ -0,0 +1,6 @@
+#include "Plane.h"
+
+float Plane::Intersection(glm::vec3 Origin, glm::vec3 Direction)
+{
+	return -1;
+}
--- 322RayTracer/Plane.h
+++ 322RayTracer/Plane.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Shape.h"
+
+class Plane : public Shape {
+public:
+	glm::vec3 Origin;
+	glm::vec3 Normal;
+	Plane(glm::vec3 o, glm::vec3 n, glm::vec3 c) :
+		Shape(c), Origin(o), Normal(n)
+	{
+	};
+	virtual float Intersection(glm::vec3 Origin, glm::vec3 Direction);
+};
