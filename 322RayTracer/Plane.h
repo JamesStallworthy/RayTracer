087361eb0ca53aae2e7717a427c04d2257b0ba71
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-10-10 15:42:52 +0100

    Phong to plane

--- 322RayTracer/Plane.h
+++ 322RayTracer/Plane.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "Shape.h"
+#include "light.h"
 
 class Plane : public Shape {
 public:
 	glm::vec3 Origin;
 	glm::vec3 Normal;
-	Plane(glm::vec3 o, glm::vec3 n, glm::vec3 c, float _Ambient) :
-		Shape(c,_Ambient), Origin(o)
+	Light* light;
+	Plane(glm::vec3 o, glm::vec3 n, glm::vec3 c, float _Ambient, Light* _light) :
+		Shape(c,_Ambient), Origin(o), light(_light)
 	{
 		Normal = glm::normalize(n);
 	};
@@ -14,4 +16,6 @@ public:
 	virtual float Intersection(glm::vec3 Origin, glm::vec3 Direction);
 	virtual glm::vec3 PhongShading(float, glm::vec3, glm::vec3, glm::vec3);
 	glm::vec3 CalcAmbient();
+	glm::vec3 CalcDiffuse(glm::vec3 l, glm::vec3 n);
+	glm::vec3 CalcSpecular(glm::vec3 l, glm::vec3 n, glm::vec3 CameraPos, glm::vec3 ContactPoint);
 };
