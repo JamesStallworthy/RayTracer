a67be8d6be48797cae98df830eb251ee02d412e3
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-01 20:22:32 +0000

    Added comments to main

--- 322RayTracer/main.cpp
+++ 322RayTracer/main.cpp
@@ -19,13 +19,14 @@ int height = 480;
 
 float DirectionCam = -1;//For camera movement
 float DirectionSphere = -1;//For ball movement
-bool CamMove = false;
-bool SphereMove = false;
+bool CamMove = false;//Camera moving
+bool SphereMove = false;// Sphere moving
 
-glm::vec3 **image = new glm::vec3*[width];
-Light light(glm::vec3(0,15,0),1);
+glm::vec3 **image = new glm::vec3*[width]; //Image array for storing pixel colours, used for saving to file
+Light light(glm::vec3(0,15,0),1);//point light
 AreaLight arealight(glm::vec3(0, 15, 0), 5, 1, 4);
-Ray ray(80, width, height, &light, &arealight, 21);
+
+Ray ray(80, width, height, &light, &arealight, 21);//Main ray tracing class
 
 //Sphere Pyramid
 Shape* One = new Sphere(glm::vec3(0, -2, -20), 2, glm::vec3(0, 0.2, 0.7),0.1,100, false, 1, 0.5);
@@ -47,8 +48,8 @@ Shape* Fourteen = new Sphere(glm::vec3(0, 4, -24), 2, glm::vec3(0, 0.2, 0.7), 0.
 Shape* Mirror1 = new Sphere(glm::vec3(15, 4, -24), 5, glm::vec3(1, 1, 0), 0.1, 10, true, 1, 0.07);
 Shape* Mirror2 = new Sphere(glm::vec3(-25, 6, -35), 5, glm::vec3(0.6, 0.6, 0.6), 0.1, 10, true, 1, 0.1);
 
+//Floor
 Shape* plane = new Plane(glm::vec3(0, -4, -20), glm::vec3(0, 1, 0), glm::vec3(0.2, 0.2, 0.2),0.1,10000, false, 1, 1);
-Shape* background = new Plane(glm::vec3(0, 0, -50), glm::vec3(0, 0, 1), glm::vec3(0, 0.7, 1), 0.1, 10000, false, 1, 1);
 
 //Triangle Pyramid
 Shape* triangle = new Triangle(glm::vec3(-10, -3, -20), glm::vec3(-20, -3, -20), glm::vec3(-15, 4, -25), glm::vec3(1, 0, 0),0.1,2, false, 1, 0.1);
@@ -57,9 +58,10 @@ Shape* triangle3 = new Triangle(glm::vec3(-20, -3, -20), glm::vec3(-20, -3, -30)
 Shape* triangle4 = new Triangle(glm::vec3(-10, -3, -30), glm::vec3(-10, -3, -20), glm::vec3(-15, 4, -25), glm::vec3(1, 0, 0), 0.1, 2, false, 1, 0.1);
 
 //Shape* poly2 = new Poly(glm::vec3(4, -3, -10), glm::vec3(4, -3, -11), glm::vec3(4, -4, -10), glm::vec3(4, -4, -11), glm::vec3(1, 1, 1), 0.1, 100, false, 1, 0.1);
-Shape* ShapeArray[22];
 
+Shape* ShapeArray[21];// Array storing all shapes
 
+//Save image to file
 void Save_Image() {
 	std::ofstream ofs("./untitled.pmm", std::ios::out | std::ios::binary);
 	ofs << "P6\n" << width << " " << height << "\n255\n";
@@ -73,11 +75,12 @@ void Save_Image() {
 	ofs.close();
 }
 
+//Create the 2D array
 void fillImageArray() {
-	//fill image array
 	for (int i = 0; i < width; i++) image[i] = new glm::vec3[height];
 }
 
+//Makes all pixels in image array black
 void clearImageArray() {
 	//fill image array
 	for (int x = 0; x < width; x++)
@@ -85,6 +88,7 @@ void clearImageArray() {
 			image[x][y] = glm::vec3(0, 0, 0);
 }
 
+//Call raycast function and time
 void render() {
 	t = clock();
 	clearImageArray();
@@ -94,7 +98,6 @@ void render() {
 }
 
 bool Controls(SDL_Window* _window) {
-
 	if (SDL_PollEvent(&event)) {
 		if (event.type == SDL_QUIT) {
 			return false;
@@ -140,6 +143,7 @@ bool Controls(SDL_Window* _window) {
 
 }
 
+//Animate the light movement	
 void movelight() {
 	if (light.Position.z < -50)
 		DirectionCam = 1;
@@ -147,7 +151,7 @@ void movelight() {
 		DirectionCam = -1;
 	light.Position.z = light.Position.z + DirectionCam;
 }
-
+//Animate the sphere movement
 void movesphere() {
 	if (Mirror2->GetOrigin().x < -30)
 		DirectionSphere = 1;
@@ -157,16 +161,24 @@ void movesphere() {
 }
 
 int main(int argc, char *argv[]) {
+
+	//SDL init
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window;
-	window = SDL_CreateWindow("Raycaster", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, NULL);
+	window = SDL_CreateWindow("Raytracer - James Stallworthy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, NULL);
 	SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+	//Pass window parameters to raytacer
 	ray.SetWindow(window, surface);
+
+	//Get soft shadow sample from user
 	int samples;
 	std::cout << "How many softshadow samples: " << std::endl;
 	std::cin >> samples;
 	std::cout << "Rendering...." << std::endl;
 	arealight.samples = samples;
+
+	//Assign shapes to the shape array
 	ShapeArray[0] = One;
 	ShapeArray[1] = Two;
 	ShapeArray[2] = Three;
@@ -188,27 +200,32 @@ int main(int argc, char *argv[]) {
 	ShapeArray[18] = triangle3;
 	ShapeArray[19] = triangle4;
 	ShapeArray[20] = Mirror2;
-	ShapeArray[21] = background;
-	bool Display = true;
-	fillImageArray();
-	SDL_Event event;
-	render();
 
+	bool Display = true;//Keep window open
+
+	fillImageArray(); //Create 2D array
+
+	SDL_Event event;
+	
+	render();//Render initial frame
+
+	//Display controls 
 	std::cout << "\n---------------------------------\n";
 	std::cout << "WASD keys to move camera \nShift and Ctrl to move up and down\nZ to activate moving light\nX to activate moving mirror sphere\nF to toggle shadows" << std::endl;
 	std::cout << "\n---------------------------------\n";
+
+
 	while (Display) {
 		//if display == false close program
-		if (CamMove)
+		if (CamMove)//If camera move, call light animation function
 			movelight();
-		if (SphereMove)
+		if (SphereMove)//If sphere move, call sphere animation function
 			movesphere();
-		//if ball is moving or cam is moving update screen every frame
-		if (CamMove||SphereMove)
+		if (CamMove||SphereMove)//if ball is moving or cam is moving update screen every frame
 			render();
 		Display = Controls(window);
 	}
-	Save_Image();
+	Save_Image(); // Save image before closing program
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
